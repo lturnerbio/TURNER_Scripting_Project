0bf951a7c1a542dfaf1256ccebe30e6e71f96293
Logan Turner <lturnerbio@gmail.com>
2021-04-05 16:01:37 -0500

    added more documentation to  cutadapt.sh

--- cutadapt.sh
+++ cutadapt.sh
@@ -7,6 +7,7 @@ module load anaconda/3-2020.02
 #-o first output and -p is paired output. Don't forget it, or the next person you help will have the same problem as you. Dumba**.
 #runs cutadapt on sample files. 
 #you will need to make sure you have the correct adapters for your organism. This script is currently set up for the adapters of Lutjanus griseus.
+#for the output files here, I added "_cutadapted" to the new filenames in order to differentiate them from the originals
 
 cutadapt -g AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -a GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG --trim-n -m 50 -q 30 --pair-filter=any --cores=0 -o /home/aublft/raw_data/B1/B1_1_cutadapted.fq.gz -p /home/aublft/raw_data/B1/B1_2cutadapted.fq.gz /home/aublft/raw_data/B1/B1_1.fq.gz /home/aublft/raw_data/B1/B1_2.fq.gz
 
