4e28c9e96b7de36878cf18cba2ed23700015302c
lturnerbio <71566373+lturnerbio@users.noreply.github.com>
2021-04-12 13:49:38 -0500

    Update transdecoder.sh

--- transdecoder.sh
+++ transdecoder.sh
@@ -6,7 +6,8 @@ source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load transdecoder/5.5.0
 
 # -t denotes number of threads(cores) to be used. Leave blank and it will take what you put into the queue
-
+# .LongOrfs tells the command to extract open reading frames
+# .Predict tells the command to predict likely coding regions from your alignment
 TransDecoder.LongOrfs -t /home/aublft/raw_data/Trinity_Files/TrinityB1/B1_trinity.Trinity.fasta
 TransDecoder.Predict -t /home/aublft/raw_data/Trinity_Files/TrinityB1/B1_trinity.Trinity.fasta
 
